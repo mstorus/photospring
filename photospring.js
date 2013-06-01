95a61ffeea253643a5211daf1d8bc178e4a73625
Mark Storus <mstorus@mixedmedialabs.com>
2013-06-01 00:00:29 -0700

    Initial commit

---
+++ photospring.js
@@ -0,0 +1,16 @@
+(function () {
+    angular.module('Photospring', []).controller("PhotospringCtrl", ["$scope", "$http", function ($scope, $http) {
+		$scope.photos = [];
+        $scope.loadFeed = function (e) {
+            var url = 'https://alpha-api.app.net/feed/rss/users/@hand_picd/posts';
+            var r = $http.jsonp('http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+            r.then(function (res) {
+				console.log(res);
+				var entries = res.data.responseData.feed.entries;
+				$scope.photos = _.map(entries, function (entry) {
+					
+				});
+            });
+        };
+    }]);
+})();
