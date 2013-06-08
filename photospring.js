1e1367eba2794d2ac41557a7fa16557cfaf0f9f9
Mark Storus <mstorus@mixedmedialabs.com>
2013-06-08 15:35:21 -0700

    select username, click on images to view full screen

--- photospring.js
+++ photospring.js
@@ -1,11 +1,12 @@
 (function () {
-    angular.module('Photospring', []).controller("PhotospringCtrl", ["$scope", "$http", function ($scope, $http) {
-		$scope.oembeds = [];
+    angular.module('Photospring', ['ui']).controller("PhotospringCtrl", ["$scope", "$http", function ($scope, $http) {
         $scope.loadFeed = function (e) {
-			var url = 'https://alpha-api.app.net/stream/0/users/@hand_picd/posts?include_post_annotations=1';
+			$scope.oembeds = [];
+			var username = $('input[name="username"]').val();
+			var url = 'https://alpha-api.app.net/stream/0/users/@' + username + '/posts?include_post_annotations=1&count=50';
             var r = $http.get(url);
             r.then(function (res) {
-				console.log(res);
+				//console.log(res);
 				var entries = res.data.data;
 				_.each(entries, function (entry) {
 					if (entry.repost_of) {
